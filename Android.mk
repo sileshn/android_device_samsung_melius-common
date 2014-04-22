e92dc48490e57a8b3863ca7cc23f8259de3f5bb8
Silesh K Nair <sileshn@gmail.com>
2014-04-22 10:58:08 +0530

    Add meliusltespr to build barrier

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-ifneq ($(filter melius3gxx meliusltexx,$(TARGET_DEVICE)),)
+ifneq ($(filter melius3gxx meliusltexx meliusltespr,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
