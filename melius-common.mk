c8a11a070a303fbf48c3a575a4120df612127796
Silesh K Nair <sileshn@gmail.com>
2014-10-05 20:14:55 +0530

    Fix commit screwup

--- melius-common.mk
+++ melius-common.mk
@@ -172,7 +172,7 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SerranoRIL
+    ro.telephony.ril_class=MeliusRIL
 
 # call common msm8930
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
