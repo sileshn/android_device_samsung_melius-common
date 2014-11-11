499987e9a7b641a35c5ac6911cb4f3f82ea23423
Arne Coucheron <arco68@gmail.com>
2014-11-11 23:33:27 +0100

    Move some common stuff to msm8930-common
    Change-Id: Iad863c9b6b1f95454446ff3bf841e4158ea865e6

--- melius-common.mk
+++ melius-common.mk
@@ -24,8 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/melius-common/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-PRODUCT_BOOT_JARS += qcmediaplayer
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
@@ -87,19 +85,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
 
 # Torch
-PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += \
+    Torch
 
 # Lights
-PRODUCT_PACKAGES += lights.msm8960
-
-# QRNGD
-PRODUCT_PACKAGES += qrngd
-
-# qcmediaplayer
-PRODUCT_PACKAGES += qcmediaplayer
+PRODUCT_PACKAGES += \
+    lights.msm8960
 
 # IR package
-PRODUCT_PACKAGES += consumerir.msm8960
+PRODUCT_PACKAGES += \
+    consumerir.msm8960
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
