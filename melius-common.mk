419732dc563b4b6c4d761d071e2066b64cd345fa
Arne Coucheron <arco68@gmail.com>
2014-12-04 07:35:06 +0100

    prop: Set default locale to en_GB
     * Most users of this device is located outside of the US, so it
       makes more sense to have a locale where 24H and metric are used.
    
    Change-Id: If031c83eea0d9264c9929f33469019b615b99cc0

--- melius-common.mk
+++ melius-common.mk
@@ -191,6 +191,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=MeliusRIL
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.locale.language=en \
+    ro.product.locale.region=GB
+
 # call common msm8930
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 
