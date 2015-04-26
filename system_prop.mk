70530df519c9d05425fadd2d899d0b9762cdfab8
Silesh K Nair <sileshn@gmail.com>
2015-04-26 21:04:01 +0400

    lcd density=280

--- system_prop.mk
+++ system_prop.mk
@@ -46,7 +46,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=c2d \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
-    ro.sf.lcd_density=240 \
+    ro.sf.lcd_density=280 \
     ro.opengles.version=196608 \
     ro.qualcomm.cabl=0
 
