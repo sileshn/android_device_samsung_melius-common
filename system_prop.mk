a56910ad4df3d351a55a8182504486b85235ccc9
Silesh K Nair <sileshn@gmail.com>
2015-05-25 14:35:09 +0530

    Revert "prop: Switch to NuPlayer"
    This reverts commit 7b7b193cd9b36787a745f0a9b8b53810bd606dbe.

--- system_prop.mk
+++ system_prop.mk
@@ -33,6 +33,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.gapless.playback.disable=true \
     audio.offload.disable=1 \
+    persist.sys.media.use-awesome=true \
     mm.enable.qcom_parser=3407871 \
     mm.enable.smoothstreaming=true \
     persist.audio.fluence.mode=endfire \
