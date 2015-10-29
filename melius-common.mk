678fa61fedd072dd1de4d03488f22f05bace635c
Arne Coucheron <arco68@gmail.com>
2015-10-29 03:02:20 +0100

    device: Enable MIDI support
    Change-Id: I7b21d612f9b1ddb128371acca9fbe3d0a9cb9c7d

--- melius-common.mk
+++ melius-common.mk
@@ -33,7 +33,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
     
 # Audio configuration
 PRODUCT_COPY_FILES += \
