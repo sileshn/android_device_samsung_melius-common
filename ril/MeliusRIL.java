fba340e50b72c2bcbb208228ee48e39fa40dc143
Arne Coucheron <arco68@gmail.com>
2015-12-15 01:03:41 +0100

    ril: Use correct preferredNetworkType instead of networkModes
    Change-Id: Icb21d2ff6c7999354a06684c10dbfe7e50bd2d8c

--- ril/MeliusRIL.java
+++ ril/MeliusRIL.java
@@ -44,8 +44,8 @@ public class MeliusRIL extends RIL {
     private static final int RIL_UNSOL_WB_AMR_STATE = 11017;
     private static final int RIL_UNSOL_RESPONSE_HANDOVER = 11021;
 
-    public MeliusRIL(Context context, int networkModes, int cdmaSubscription) {
-        this(context, networkModes, cdmaSubscription, null);
+    public MeliusRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
+        this(context, preferredNetworkType, cdmaSubscription, null);
     }
 
     public MeliusRIL(Context context, int preferredNetworkType,
