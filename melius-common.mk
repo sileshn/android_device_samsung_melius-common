5254735d2ea2b853a268a630909028c6a83bed02
Silesh K Nair <sileshn@gmail.com>
2015-12-26 01:12:16 +0530

    Remove init.qcom.fm.sh

--- melius-common.mk
+++ melius-common.mk
@@ -82,8 +82,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/init.crda.sh:system/etc/init.crda.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.audio.sh:system/etc/init.qcom.audio.sh \
-    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
+    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
 # GPS/location security configuration file
 PRODUCT_COPY_FILES += \
