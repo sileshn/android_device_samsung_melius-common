cb834b3ee98aec8e7f1aa23d0159ceec1a10ad86
Arne Coucheron <arco68@gmail.com>
2016-02-08 05:59:53 +0100

    rootdir: Remove obsolete crda service
    Change-Id: Ia0a31a2bdf1de5029008d4b2134390a5c98c1f21

--- melius-common.mk
+++ melius-common.mk
@@ -81,7 +81,6 @@ PRODUCT_COPY_FILES += \
 
 # Etc scripts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/init.crda.sh:system/etc/init.crda.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.audio.sh:system/etc/init.qcom.audio.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
