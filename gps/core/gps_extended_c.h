9195e90289c30c2c20c939f47cee636e856b63b4
Vamana Murthi <vthuniki@codeaurora.org>
2016-02-29 15:06:13 +0530

    melius-common: gps: NHz: Update TBF limit from 1 sec to .1sec
    Updated minimum supported fix interval from
    1000ms to 100 ms
    
    Change-Id: Idd1e4a5bdf862287450dc517e737490f2ef77433
    CRs-Fixed: 979702

--- gps/core/gps_extended_c.h
+++ gps/core/gps_extended_c.h
@@ -183,7 +183,7 @@ typedef enum loc_position_mode_type {
     LOC_POSITION_MODE_RESERVED_5
 } LocPositionMode;
 
-#define MIN_POSSIBLE_FIX_INTERVAL 1000 /* msec */
+#define MIN_POSSIBLE_FIX_INTERVAL 100 /* msec */
 
 /** Flags to indicate which values are valid in a GpsLocationExtended. */
 typedef uint16_t GpsLocationExtendedFlags;
