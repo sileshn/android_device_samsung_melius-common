dfc01ef8dc98d8715ee87fa63377b240f7a706b0
Arne Coucheron <arco68@gmail.com>
2016-05-11 00:08:36 +0200

    audio: Changes for new audio HAL
     * Deprecate old defines
     * Deprecate old system props and use new ones
     * Add custom audio_platform_info.xml
     * Add custom mixer_paths.xml
     * Change audio_policy.conf for new HAL
     * Remove old sound use case file
    
    Change-Id: I3b180427743f56b384a72f948948a357ae7310d4

--- melius-common.mk
+++ melius-common.mk
@@ -39,8 +39,10 @@ PRODUCT_COPY_FILES += \
     
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media Profile
 PRODUCT_COPY_FILES += \
@@ -155,6 +157,10 @@ PRODUCT_PACKAGES += \
 # OpenCamera
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/net.sourceforge.opencamera.apk:system/priv-app/OpenCamera/OpenCamera.apk
+
+# Voice processing
+PRODUCT_PACKAGES += \
+    libqcomvoiceprocessing
 
 # call common melius system props
 $(call inherit-product, device/samsung/melius-common/system_prop.mk)
