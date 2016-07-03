c34ecbff522a08ebc5824b54cf50f6e31bcac57c
Arne Coucheron <arco68@gmail.com>
2016-07-04 01:23:54 +0200

    audio: Update configuration
    * Use deep buffer as primary output
    * Use "BT Nrec" ACDBID for BT headset and mic
    * Add multi-channel support
    * Add missing voice_rx definition
    
    Change-Id: Icbad784a2d7561570ddfbdfffa741bfb8eb9a171

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/melius-common/bluetooth
 
 # Audio
-AUDIO_USE_LL_AS_PRIMARY_OUTPUT := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
 # Allow suspend in charge mode
