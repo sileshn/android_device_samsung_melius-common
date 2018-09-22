bc4a305597ef5ca24cc72c5247b3ce585149aab7
Silesh K Nair <sileshn@gmail.com>
2018-09-22 08:28:48 +0530

    melius-common: s/ro.vendor.product/ro.product.vendor/g
    Prefixing with ro.product.vendor is the correct way.
    
    Change-Id: I38a5b7217d1ac66412219f93d895c6aa0a65abe3

--- init/init_melius.cpp
+++ init/init_melius.cpp
@@ -42,38 +42,38 @@ void vendor_load_properties()
         /* melius3gxx */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/melius3gxx/melius3g:4.4.2/KOT49H/I9200XXUDNE4:user/release-keys");
         property_override("ro.build.description", "melius3gxx-user 4.4.2 KOT49H I9200XXUDNE4 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-I9200");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "melius3gxx");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "GT-I9200");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "melius3gxx");
     } else if (model == "I9208") {
         /* melius3gzm */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/melius3gzm/melius3g:4.2.2/JDQ39/I9208ZMUCNB2:user/release-keys");
         property_override("ro.build.description", "melius3gzm-user 4.2.2 JDQ39 I9208ZMUCNB2 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-I9208");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "melius3gzm");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "GT-I9208");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "melius3gzm");
     } else if (model == "I9205") {
         /* meliusltexx */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/meliusltexx/meliuslte:4.4.2/KOT49H/I9205XXUDNE4:user/release-keys");
         property_override("ro.build.description", "meliusltexx-user 4.4.2 KOT49H I9205XXUDNE4 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-I9205");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "meliusltexx");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "GT-I9205");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "meliusltexx");
     } else if (model == "L600") {
         /* meliusltespr */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/meliusltespr/meliusltespr:4.4.2/KOT49H/L600VPUBOH1:user/release-keys");
         property_override("ro.build.description", "meliusltespr-user 4.4.2 KOT49H L600VPUBOH1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SPH-L600");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "meliusltespr");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "SPH-L600");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "meliusltespr");
     } else if (model == "I527M") {
         /* meliusltecan */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/meliusltevl/meliusltecan:4.2.2/JDQ39/I527MVLUBNA3:user/release-keys");
         property_override("ro.build.description", "meliusltevl-user 4.2.2 JDQ39 I527MVLUBNA3 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SGH-I527M");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "meliusltecan");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "SGH-I527M");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "meliusltecan");
     } else if ((model == "M819N") || (model == "M819NUVUBNI5")) {
         /* meliuslteMetroPCS */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/meliuslteMetroPCS/meliuslteMetroPCS:4.2.2/JDQ39/M819NUVUAMK7:user/release-keys");
         property_override("ro.build.description", "meliuslteMetroPCS-user 4.2.2 JDQ39 M819NUVUAMK7 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SGH-M819N");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "meliuslteMetroPCS");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "SGH-M819N");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "meliuslteMetroPCS");
     }
 
     const std::string device = android::base::GetProperty("ro.product.device", "");
