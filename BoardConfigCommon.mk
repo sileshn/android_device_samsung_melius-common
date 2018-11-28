6247ae9df4abbedb94faa5e62f80fcf506ecf2ec
Arne Coucheron <arco68@gmail.com>
2018-11-28 02:27:20 +0100

    melius-common: Move common platform manifest and matrix to msm8930-common
    Change-Id: I42216242352b22244cf701daf5871bb0a7a98b09

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,8 +29,7 @@ COMMON_PATH := device/samsung/melius-common
 PRODUCT_SHIPPING_API_LEVEL := 19
 
 # HIDL
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Set default locale
 PRODUCT_LOCALES := en-GB
