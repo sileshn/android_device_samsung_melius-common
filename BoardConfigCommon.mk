370f7887235621bbb2611a32c3c84f77b345f576
Bruno Martins <bgcngm@gmail.com>
2019-01-15 00:24:17 +0000

    melius-common: Replace KeyDisabler class with Touch HAL
    Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -90,9 +91,6 @@ TARGET_PROVIDES_LIBLIGHT := true
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 TARGET_RIL_VARIANT := caf
-
-# LineageHW
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(COMMON_PATH)/lineagehw|**/*.java
 
 # TWRP
 ifeq ($(WITH_TWRP),true)
