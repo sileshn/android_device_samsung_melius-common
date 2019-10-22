6c5690f4bb83c4ce57b1c4b9b788beaffdfda13a
Wang Han <416810799@qq.com>
2019-10-22 14:18:55 +0800

    melius-common: Remove deprecated ipv6 tethering support
     * This is not a thing anymore since N.
    
    Change-Id: I944115cc8a33bd3ea21cb9c64c9c98a97d8f0373

--- melius-common.mk
+++ melius-common.mk
@@ -143,10 +143,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.melius
 
-# IPv6 tethering
-PRODUCT_PACKAGES += \
-    ebtables
-
 # F2FS
 PRODUCT_PACKAGES += \
     fsck.f2fs \
