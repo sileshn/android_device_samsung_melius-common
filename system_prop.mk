d79d02776f7ffa3b2d8f48b4a23ddeab7a6e7116
Arne Coucheron <arco68@gmail.com>
2019-12-29 22:16:36 +0100

    melius-common: Remove obsolete media props
    Change-Id: I5b685c26d12899a74e97164c24bd4deeaf20f5ab

--- system_prop.mk
+++ system_prop.mk
@@ -14,10 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.disable=1 \
     debug.stagefright.ccodec=0 \
-    mm.enable.smoothstreaming=true \
     vendor.dedicated.device.for.voip=false \
-    vendor.voice.path.for.pcm.voip=false \
-    media.aac_51_output_enabled=true
+    vendor.voice.path.for.pcm.voip=false
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
