aeb42a8397f4ea14320c5bd51269bf35bae0f0ae
Paul Keith <javelinanddart@gmail.com>
2020-02-01 02:52:54 +0100

    melius-common: Transition to consumerir HIDL hal
    Change-Id: I76960189fef5fdef7309b50d1a772110723350f4

--- melius-common.mk
+++ melius-common.mk
@@ -123,8 +123,7 @@ PRODUCT_PACKAGES += \
 
 # IR package
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    consumerir.msm8960
+    android.hardware.ir@1.0-service.samsung
 
 # Vibrator
 PRODUCT_PACKAGES += \
