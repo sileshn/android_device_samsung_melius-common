254ba037dfaf4799205356bd1a640a6c9a6f8b22
Tobias Tefke <tobias@tefke.name>
2020-03-14 18:46:41 +0100

    Update twrp config
    * use common guard
    * remove crypto for now (otherwise the twrp image will be bigger than the recovery partition)
      TODO: revisit later
    
    Change-Id: Id0350577c8d9c6e4d061bd2fac5513dffe799a69

--- twrp.mk
+++ twrp.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(MELIUS_WITH_TWRP),true)
+ifeq ($(WITH_TWRP),true)
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_VARIANT := twrp
@@ -20,7 +20,7 @@ TARGET_RECOVERY_FSTAB := device/samsung/melius-common/rootdir/twrp.fstab
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_TWRPAPP := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_TARGET_USES_QCOM_BSP := true
 TW_EXCLUDE_SUPERSU := true
