6ad9b57d613750d6009d6174166bc96ee2fe882c
Arne Coucheron <arco68@gmail.com>
2020-05-08 05:03:01 +0200

    melius-common: Minimize logspam from the QMI subsystem
    Change-Id: I2135bb4a4942ec097e8d359bd8bdce9bbc324ced

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,7 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.mobiledata=false \
+	persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.no_wait_for_card=1
